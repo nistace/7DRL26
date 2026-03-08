f1b2a451a8b2e8503da06fa0e020172a643b75db
nathan.istace <nathan.istace@gmail.com>
2026-03-08 02:26:15 +0100

    Added some sounds when changing states Fixed some equipment descriptions

---
+++ UnityProject/Assets/Scripts/SDRL26/Audio/StateStartAudioTrigger.cs
@@ -0,0 +1,31 @@
+﻿using SDRL26.GameControllers.GameStates;
+using UnityEngine;
+
+namespace SDRL26.Audio
+{
+   public class StateStartAudioTrigger : MonoBehaviour
+   {
+      [SerializeField] private AudioSource _source;
+      [SerializeField] private StateDataList<AudioClip> _audioClips;
+
+      private void Start()
+      {
+         GameState.OnStateChanged.AddListener(HandleStateChanged);
+      }
+
+      private void OnDestroy()
+      {
+         GameState.OnStateChanged.RemoveListener(HandleStateChanged);
+      }
+
+      private void HandleStateChanged(GameState state)
+      {
+         var clip = _audioClips.First(state.Types);
+
+         if (clip != null)
+         {
+            _source.PlayOneShot(clip);
+         }
+      }
+   }
+}
